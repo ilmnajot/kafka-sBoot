383aa79e1f27b98e8137f6311386a0c89b75c2b6
macbookpro <ilmnajot2021@gmail.com>
2025-09-14 07:33:24 +0500

    first commit

---
+++ src/main/java/uz/ilmnajot/kafkasboot/KafkaSBootApplication.java
@@ -0,0 +1,13 @@
+package uz.ilmnajot.kafkasboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaSBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/ilmnajot/kafkasboot/KafkaSBootApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.ilmnajot.kafkasboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
